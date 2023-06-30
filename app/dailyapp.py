f02deef44f2fb553c3a3370b7415c0b794c8faab
Mark Borysiak <borysiak.1@gmail.com>
2023-06-29 17:26:50 -0700

    Update dailyapp.py

--- app/dailyapp.py
+++ app/dailyapp.py
@@ -351,12 +351,11 @@ def main():
 
     data_class = PullData(week, year, db_name, op_params)
     player_data, covar, min_max = data_class.pull_player_data()
-    st.write(player_data)
     display_data = get_display_data(player_data)
-    st.write(display_data.dtypes)
-    st.write(data_class.pred_vers, data_class.ensemble_vers, data_class.std_dev_type)
-    st.write(data_class.covar_type, data_class.full_model_weight)
-    st.write(sim.num_iters, sim.use_ownership, sim.salary_remain_max)
+    
+    # st.write(data_class.pred_vers, data_class.ensemble_vers, data_class.std_dev_type)
+    # st.write(data_class.covar_type, data_class.full_model_weight)
+    # st.write(sim.num_iters, sim.use_ownership, sim.salary_remain_max)
     
     sim = init_sim(player_data, covar, min_max, data_class.use_covar, op_params, pos_require_start)
 
