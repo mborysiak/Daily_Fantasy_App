05a24b9e9f05d8db10b796c771dce2ed253a01c7
Mark Borysiak <borysiak.1@gmail.com>
2025-10-25 11:01:24 -0400

    week 8

--- app/dailyapp.py
+++ app/dailyapp.py
@@ -11,7 +11,7 @@ from pathlib import Path
 from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, select, DateTime
 
 year = 2025
-week = 7
+week = 8
 total_lineups = 10
 db_name = 'Simulation_App.sqlite3'
 
